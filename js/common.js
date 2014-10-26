aa89f6a1e8778de8423100cebf79e88b1cf22f0b
sputman13 <sputman@wowway.com>
2014-10-26 10:53:25 -0400

    Initial commit
    Placing files into the repository.

---
+++ js/common.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var tincan = new TinCan (
+    {
+        recordStores: [
+            {
+                endpoint: "<Enter endpoint URL here>",
+                username: "<Enter username here>",
+                password: "<Enter password here>"
+            }
+        ]
+    }
+);
---
+++ js/config.js
@@ -0,0 +1,9 @@
+//globals: equal, responseText, statement, ok, deepEqual, QUnit, module, asyncTest, Util, start, golfStatements, console
+/*jslint bitwise: true, browser: true, plusplus: true, maxerr: 50, indent: 4 */
+function Config() {
+	"use strict";
+}
+Config.endpoint = "https://cloud.scorm.com/tc/CELRXWK7LQ/";
+Config.authUser = "putman@altair.com";
+Config.authPassword = "71Chevelle";
+Config.actor = { "mbox":["test@user.com"], "name":["Test User"] };
